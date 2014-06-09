09ce1308f034ea607a47f09e99f4e2ea83c7cce8
Banpot Srihawong <banpot.sr@gmail.com>
2014-06-10 06:03:06 +0700

    ads full page loading progress

---
+++ amazingthailand/src/main/java/info/srihawong/amazingthailand/Config.java
@@ -0,0 +1,11 @@
+package info.srihawong.amazingthailand;
+
+/**
+ * Created by Demo on 6/9/14 AD.
+ */
+public class Config {
+    public static final String adsInterstitialUnitIDFullPage = "ca-app-pub-4631743491157031/4839789078";
+    public static final String adsInterstitialUnitIDTop = "ca-app-pub-4631743491157031/3363055876";
+    public static final String adsInterstitialKeyword = "booking, fight, ticket, promotion";
+    public static final int adsInterstitialPageShow = 2;
+}
